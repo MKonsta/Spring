28ddbada20f4578d62014f2100b07099fbcfc98b
Konstantin <kostyan.exe@gmail.com>
2022-08-18 09:34:11 +0300

    first_commit

---
+++ jdbcVsSpringData/src/test/java/com/kos/jdbcVsSpringData/JdbcVsSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kos.jdbcVsSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcVsSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
