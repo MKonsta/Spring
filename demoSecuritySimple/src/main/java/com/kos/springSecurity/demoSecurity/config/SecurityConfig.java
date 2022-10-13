13699889920a9de6061e4f2a7c4d861cd75ca7e4
Konstantin <kostyan.exe@gmail.com>
2022-10-13 12:20:15 +0300

    add authorization

--- demoSecuritySimple/src/main/java/com/kos/springSecurity/demoSecurity/config/SecurityConfig.java
+++ demoSecuritySimple/src/main/java/com/kos/springSecurity/demoSecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.kos.springSecurity.demoSecurity.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configurer(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    // Настраиваем аутентификацию
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
 
