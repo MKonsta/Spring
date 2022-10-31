3376ab152db02e7bef9bed0e96ba1ae0329a294d
Konstantin <kostyan.exe@gmail.com>
2022-10-31 09:53:49 +0300

    add preAuthorise

--- demoSecuritySimple/src/main/java/com/kos/springSecurity/demoSecurity/controllers/HelloController.java
+++ demoSecuritySimple/src/main/java/com/kos/springSecurity/demoSecurity/controllers/HelloController.java
@@ -1,13 +1,33 @@
 package com.kos.springSecurity.demoSecurity.controllers;
 
+import com.kos.springSecurity.demoSecurity.models.Person;
 import com.kos.springSecurity.demoSecurity.security.PersonDetails;
+import com.kos.springSecurity.demoSecurity.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/")
+    public String startPage() {
+        return "redirect:/hello";
+    }
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -28,5 +48,11 @@ public class HelloController {
     @GetMapping("/admin")
     public String adminPage() {
         return "admin";
+    }
+
+    @GetMapping("/secondAdmin")
+    public String secondAdminPage() {
+        adminService.doAdminStuff();
+        return "second_admin";
     }
 }
